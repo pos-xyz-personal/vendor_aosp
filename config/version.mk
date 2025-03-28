50bf5159d1b5ffc73f8814785508498e7e616731
alternoegraha <noegrahachan@gmail.com>
2025-03-28 16:28:03 +0700

    config: Setup custom OTA
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -13,11 +13,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.custom.version=$(CUSTOM_VERSION)
 
 # Updater
-ifeq ($(IS_OFFICIAL),true)
-    PRODUCT_PRODUCT_PROPERTIES += \
-        net.pixelos.build_type=ci \
-        net.pixelos.version=$(CUSTOM_VERSION_PROP)
-endif
+PRODUCT_PRODUCT_PROPERTIES += \
+    net.pixelos.build_type=ci \
+    net.pixelos.version=$(CUSTOM_VERSION_PROP)
 
 # Signing
 ifneq (eng,$(TARGET_BUILD_VARIANT))
